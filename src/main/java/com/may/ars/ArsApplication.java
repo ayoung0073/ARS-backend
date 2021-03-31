a3452d1bb13aff6f35afb87a64346c935f9fad28
ayoung0073 <ayong0310@naver.com>
2021-03-31 16:46:21 +0900

    Chore : Project Setting

---
+++ src/main/java/com/may/ars/ArsApplication.java
@@ -0,0 +1,13 @@
+package com.may.ars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/may/ars/ArsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.may.ars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
