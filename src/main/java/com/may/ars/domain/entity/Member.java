25b7f8cf59eccc13b455a5e92726baaad786adc1
ayoung0073 <ayong0310@naver.com>
2021-03-31 21:34:54 +0900

    Chore : Database Setting

---
+++ src/main/java/com/may/ars/domain/entity/Member.java
@@ -0,0 +1,28 @@
+package com.may.ars.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String socialType;
+
+    private String nickname;
+}
---
+++ src/main/java/com/may/ars/domain/entity/MemberRepository.java
@@ -0,0 +1,6 @@
+package com.may.ars.domain.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
