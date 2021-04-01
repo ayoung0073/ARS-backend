ff67d7de1ab3aec48d9935e0829739d4fff70819
ayoung0073 <ayong0310@naver.com>
2021-04-01 14:11:30 +0900

    Add : Login View

---
+++ src/main/java/com/may/ars/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.may.ars.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "/user/login";
+    }
+}
