7365c2ad555a24444014645191ec10da1ad4ec53
ayoung0073 <ayong0310@naver.com>
2021-04-01 14:11:16 +0900

    Add : Layout View with mustache

---
+++ src/main/java/com/may/ars/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.may.ars.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("test", "test");
+
+        return "index";
+    }
+}
