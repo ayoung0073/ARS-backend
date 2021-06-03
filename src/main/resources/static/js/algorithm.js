c0a6fd6151ecad996bcf6311a01f62f4e846da58
ayoung0073 <ayong0310@naver.com>
2021-06-04 01:18:47 +0900

    Add : 리뷰 등록 화면 구현

--- src/main/resources/static/js/algorithm.js
+++ src/main/resources/static/js/algorithm.js
@@ -3,6 +3,9 @@ let index = {
         $("#btn-register").on("click", () => {
             this.register();
         });
+        $("#btn-review-register").on("click", () => {
+            this.reviewRegister();
+        });
     },
 
     register: function () {
@@ -41,6 +44,36 @@ let index = {
         });
     },
 
-}
+    reviewRegister: function () {
+
+        let data = {
+            content: document.getElementsByClassName("tui-editor-contents")[0].innerHTML,
+            step: $("#step").val(),
+        }
+
+        console.log(data);
+        console.log(sessionStorage.getItem("access_token"));
+
+        let problemId = document.getElementById("problem-id").value;
+        console.log(problemId) // 테스트용
+
+        $.ajax({
+            type: "POST",
+            url: "/api/problems/" + problemId + "/reviews",
+            headers: {"Authorization": sessionStorage.getItem("access_token"), "Content-type": "application/json"},
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function (result) {
+            console.log(result);
+            alert("글쓰기가 완료되었습니다.");
+            location.href = "/problems/" + problemId + "?index=1";
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+
+
+    }
 
 index.init()
