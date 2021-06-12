78b9acf5a0b301f6f79b21a8e38f5bf59d497e5d
ayoung0073 <ayong0310@naver.com>
2021-06-13 01:50:08 +0900

    Test : Review 등록 서비스 테스트
    - 성공, 실패 경우

---
+++ src/test/java/com/may/ars/service/ReviewServiceTest.java
@@ -0,0 +1,118 @@
+package com.may.ars.service;
+
+import com.may.ars.dto.member.MemberDto;
+import com.may.ars.dto.problem.ReviewRegisterDto;
+import com.may.ars.model.entity.member.Member;
+import com.may.ars.model.entity.problem.Problem;
+import com.may.ars.model.entity.problem.ProblemRepository;
+import com.may.ars.model.entity.problem.ReviewRepository;
+import com.may.ars.response.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class ReviewServiceTest {
+
+    @InjectMocks
+    private ReviewService reviewService;
+
+    @Mock
+    private ReviewRepository reviewRepository;
+
+    @Mock
+    private ProblemRepository problemRepository;
+
+    void setup() {
+        final Member member = Member.builder()
+                .email("test")
+                .id(1L)
+                .nickname("test")
+                .build();
+
+        final Problem problem = Problem.builder()
+                .id(1L)
+                .writer(member)
+                .build();
+
+        given(problemRepository.findById(1L)).willReturn(Optional.of(problem));
+    }
+
+    @Test
+    @DisplayName("리뷰 등록 테스트")
+    void 리뷰_등록_성공_테스트() {
+        setup();
+        // given
+        final MemberDto memberDto = MemberDto.builder()
+                .memberId(1L)
+                .build();
+
+        Long problemId = 1L;
+
+        final ReviewRegisterDto registerDto = ReviewRegisterDto.builder()
+                .content("리뷰 등록 테스트")
+                .step(3)
+                .build();
+
+        // when
+        reviewService.registerReview(problemId, registerDto, memberDto);
+    }
+
+    @Test
+    @DisplayName("리뷰 등록 실패 테스트 - 문제 존재하지 않는 경우")
+    void 리뷰_등록_문제X_테스트() {
+        //given
+        given(problemRepository.findById(2L)).willReturn(Optional.empty());
+        final MemberDto memberDto = MemberDto.builder()
+                .memberId(2L)
+                .build();
+
+        Long problemId = 2L;
+
+        final ReviewRegisterDto registerDto = ReviewRegisterDto.builder()
+                .content("리뷰 등록 테스트")
+                .step(3)
+                .build();
+
+        // when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> reviewService.registerReview(problemId, registerDto, memberDto)); // 예외가 발생해야 한다.
+
+        //then
+        assertThat(e.getMessage(), is(ErrorMessage.NOT_EXIST_PROBLEM));
+    }
+
+    @Test
+    @DisplayName("리뷰 등록 실패 테스트 - 권한 없는 경우")
+    void 리뷰_등록_권한X_테스트() {
+        setup();
+        //given
+        final MemberDto memberDto = MemberDto.builder()
+                .memberId(2L)
+                .build();
+
+        Long problemId = 1L;
+
+        final ReviewRegisterDto registerDto = ReviewRegisterDto.builder()
+                .content("리뷰 등록 테스트")
+                .step(3)
+                .build();
+
+        // when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> reviewService.registerReview(problemId, registerDto, memberDto)); // 예외가 발생해야 한다.
+
+        //then
+        assertThat(e.getMessage(), is(ErrorMessage.NOT_VALID_USER));
+    }
+}
