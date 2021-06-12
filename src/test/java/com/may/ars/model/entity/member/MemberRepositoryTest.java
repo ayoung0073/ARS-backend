64d45a8e7db7cb1086bc25924125e28e7c504932
ayoung0073 <ayong0310@naver.com>
2021-06-13 01:54:01 +0900

    Test : 멤버 레포지토리 테스트 추가

---
+++ src/test/java/com/may/ars/model/entity/member/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.may.ars.model.entity.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void 멤버_저장_테스트() {
+        // given
+        Member member = Member.builder()
+                .email("ayong0310@naver. com")
+                .nickname("문아영")
+                .build();
+
+        // when
+        memberRepository.save(member);
+
+        // then
+        assertThat(memberRepository.findByEmail(member.getEmail()).isPresent()).isEqualTo(true);
+    }
+}
