e5dc924ecb6a2172ce711acadc0706ef8e3f9c02
ayoung0073 <ayong0310@naver.com>
2021-06-21 22:50:33 +0900

    Fix : 메세지 전송 postMessage -> postEphemeral

--- src/main/java/com/may/ars/service/SlackBotService.java
+++ src/main/java/com/may/ars/service/SlackBotService.java
@@ -1,10 +1,12 @@
 package com.may.ars.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class SlackBotService {
 
@@ -14,18 +16,18 @@ public class SlackBotService {
     public void test() {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-        String url = "https://slack.com/api/chat.postMessage";
+        String url = "https://slack.com/api/chat.postEphemeral";
         headers.add("Authorization", "Bearer " + slackToken);
         headers.add("Content-type", "application/json");
-        String body = "{\"channel\":\"todo\", \"text\" : \"test\"}";
+        String body = "{\"channel\":\"todo\", \"text\" : \"test\", \"user\" : \"U01KYPCH6G5\"}";
 
         HttpEntity<String> requestEntity = new HttpEntity<>(body, headers);
         ResponseEntity<String> responseEntity = rest.exchange(url, HttpMethod.POST, requestEntity, String.class);
         HttpStatus httpStatus = responseEntity.getStatusCode();
         int status = httpStatus.value();
         String response = responseEntity.getBody();
-        System.out.println("Response status: " + status);
-        System.out.println(response);
+        log.info("Response status: " + status);
+        log.info(response);
     }
 
 }
