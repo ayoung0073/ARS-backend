b4c7735e60213249659bf8b7635dc745c85a1d5e
ayoung0073 <ayong0310@naver.com>
2021-06-25 15:56:18 +0900

    [#17] 리뷰 수정 페이지, API 적용

--- src/main/resources/static/js/problem.js
+++ src/main/resources/static/js/problem.js
@@ -6,13 +6,6 @@ let index = {
         $("#btn-review-register").on("click", () => {
             this.registerReview();
         });
-
-        $("#btn-problem-update-register").on("click", (problemId) => {
-            this.updateProblem(problemId);
-        });
-        $("#btn-problem-delete").on("click", (problemId) => {
-            this.deleteProblem(problemId);
-        });
     },
 
     register: function () {
@@ -126,8 +119,65 @@ let index = {
         });
     },
 
+     updateReview: function (problemId, reviewId) {
+         let tagList = [];
+         let tagArr = document.getElementsByClassName("btn-tag");
+         let step = $("#step").val();
+         let notificationDate = date_setting(step);
+         console.log(step)
+         if (isNaN(step)) { // 숫자가 아닌 경우, 즉 날짜인 경우
+             console.log(step);
+             step = $("review-step").val();
+             console.log(step);
+         }
+         console.log(document.getElementsByClassName("tui-editor-contents")[0]);
+         for (let i = 0; i < tagArr.length; i++) {
+             tagList.push(tagArr[i].value);
+         }
+
+         let data = {
+             title: $("#title").val(),
+             content: document.getElementsByClassName("tui-editor-contents")[0].innerHTML,
+             link: $("#link").val(),
+             notificationDate: notificationDate,
+             tagList: tagList,
+             step: step
+         }
+
+            $.ajax({
+                type: "PUT",
+                url: "/api/problems/" + problemId + "/reviews/" + reviewId,
+                headers: {"Authorization": sessionStorage.getItem("access_token"), "Content-type": "application/json"},
+                data: JSON.stringify(data),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json"
+            }).done(function (result) {
+                console.log(result);
+                alert("글 수정이 완료되었습니다.");
+                location.href = "/problems/" + problemId + "?index=1";
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            });
+        },
+
+        deleteReview: function (problemId) {
+            $.ajax({
+                type: "DELETE",
+                url: "/api/problems/" + problemId,
+                headers: {"Authorization": sessionStorage.getItem("access_token"), "Content-type": "application/json"},
+                dataType: "json"
+            }).done(function (result) {
+                console.log(result);
+                alert("글 삭제 완료하였습니다.");
+                location.href = "/";
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            });
+        },
+
 
 }
+
 function date_setting(step) {
     console.log(step);
     let today = new Date();
