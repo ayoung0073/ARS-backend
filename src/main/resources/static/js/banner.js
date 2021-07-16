32566174ea9a2c8fe3cd26fae513166c57a93ed7
ayoung0073 <ayong0310@naver.com>
2021-07-16 18:09:41 +0900

    [#28] 배너 수정 [FRONT]

--- src/main/resources/static/js/banner.js
+++ src/main/resources/static/js/banner.js
@@ -5,7 +5,7 @@ if (nickname == null) {
         `);
 } else {
     document.write(
-        `<span style="float: right;">&nbsp;&nbsp;<b>`
+        `<span style="position: absolute; right: 0px; padding: 7px;">&nbsp;&nbsp;<b>`
         + nickname
         + `</b>님 환영합니다!&nbsp;&nbsp;</span>`
     );
