bebf46fcc01d02895a8e75cf02d45318c979f4f0
ayoung0073 <ayong0310@naver.com>
2021-08-07 17:33:39 +0900

    [#49] 스크립트 오타 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,7 +11,7 @@ REPOSITORY=/home/ec2-user/deploy
 BUILD_JAR=$(ls /home/ec2-user/jenkins/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 
-echo "> Build 파일 복사"  >> /home/ec2-user/log/deploy.log
+echo "> Build 파일 복사 : cp $BUILD_JAR $DEPLOY_PATH"  >> /home/ec2-user/log/deploy.log
 cp $BUILD_JAR $DEPLOY_PATH 	# 새로운 jar 파일을 덮어쓴다.
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
@@ -20,10 +20,12 @@ JAR_NAME=$(ls -tr $DEPLOY_PATH*.jar | tail -n 1)
 
 IDLE_PROFILE=$(find_idle_profile)
 
-echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."  >> /home/ec2-user/log/deploy.log
+echo "> $DEPLOY_PATH 의 $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."  >> /home/ec2-user/log/deploy.log
+echo "> cd $DEPLOY_PATH ; docker build -t ars ./"  >> /home/ec2-user/log/deploy.log
 
 cd $DEPLOY_PATH
 
 docker build -t ars ./
+echo "> docker run -it --name $IDLE_PROFILE -d -e active=$IDLE_PROFILE -p $IDLE_PORT:$IDLE_PORT ars" >> /home/ec2-user/log/deploy.log
 docker run -it --name $IDLE_PROFILE -d -e active=$IDLE_PROFILE -p $IDLE_PORT:$IDLE_PORT ars
 echo ">  Success Docker run"  >> /home/ec2-user/log/deploy.log
