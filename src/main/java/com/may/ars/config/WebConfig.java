26ac5ebae908e4b2b8f790a29683d47087c8e354
ayoung0073 <ayong0310@naver.com>
2021-08-28 22:38:22 +0900

    [#44] 머스테치 제거

--- src/main/java/com/may/ars/config/WebConfig.java
+++ src/main/java/com/may/ars/config/WebConfig.java
@@ -10,18 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        MustacheViewResolver resolver = new MustacheViewResolver();
-
-        resolver.setCharset("UTF-8");
-        resolver.setContentType("text/html;charset=UTF-8");
-        resolver.setPrefix("classpath:/templates/");
-        resolver.setSuffix(".html");
-
-        registry.viewResolver(resolver);
-    }
-
-    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
--- src/main/resources/static/js/banner.js
+++
@@ -1,8 +0,0 @@
-let nickname = sessionStorage.getItem("nickname");
-if (nickname != null) {
-    document.write(
-        `<span style="position: absolute; right: 0px; padding: 7px;">&nbsp;&nbsp;<b>`
-        + nickname
-        + `</b>님 환영합니다!&nbsp;&nbsp;</span>`
-    );
-}
